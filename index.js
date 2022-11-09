40c190be4e799ac1cdb8c22231d38c0504c93139
Sharif101 <sharif009009@gmail.com>
2022-11-09 19:05:13 +0600

    server added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 require("dotenv").config();
 const app = express();
@@ -9,6 +10,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// ----------------------------------
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hm5pps9.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// ----------------------------------
 app.get("/", (req, res) => {
   res.send("server is runnig");
 });
